d94b425093e73602dfcf6d1f8aca621fe018fe02
Arya <aryailham05@gmail.com>
2024-02-08 15:34:45 +0700

    Initial commit

---
+++ MoneyManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoneyManager
+//
+//  Created by Arya Ilham on 08/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
